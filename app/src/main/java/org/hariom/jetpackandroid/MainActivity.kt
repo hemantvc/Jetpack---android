8abf70a7053f7cf7c64fb3ccb82c3a26a411dd6b
Hemant <hemant@unoindia.co>
2022-05-30 16:26:39 +0530

    Databinding example

--- app/src/main/java/org/hariom/jetpackandroid/MainActivity.kt
+++ app/src/main/java/org/hariom/jetpackandroid/MainActivity.kt
@@ -1,39 +1,20 @@
 package org.hariom.jetpackandroid
 
-import android.app.Application
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import org.hariom.jetpackandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainViewModel: MainViewModel
-    private val tvFactText : TextView
-        get() = findViewById(R.id.tvFactText)
-    private val btnUpdate: Button
-        get() = findViewById(R.id.btnUpdate)
-
-
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        mainViewModel.factsLiveData.observe(this, Observer {
-            // Code execute
-            tvFactText.text = it
-        })
-
-        btnUpdate.setOnClickListener{
-            mainViewModel.updateLiveData()
-        }
+        val quoteObj = Quote("Hariom", "Asharam bapu")
+        binding.quote = quoteObj
 
     }
-
 
 }
