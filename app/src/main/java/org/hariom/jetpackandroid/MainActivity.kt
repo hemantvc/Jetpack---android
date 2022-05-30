4da95ad204f09680438c7ec622f54b2382e5b991
Hemant <hemant@unoindia.co>
2022-05-30 18:55:30 +0530

    database complete example

--- app/src/main/java/org/hariom/jetpackandroid/MainActivity.kt
+++ app/src/main/java/org/hariom/jetpackandroid/MainActivity.kt
@@ -2,10 +2,16 @@ package org.hariom.jetpackandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+/***
+ * https://www.youtube.com/watch?v=yPL13Iwy6oM&list=PLRKyZvuMYSIO0jLgj8g6sADnD0IBaWaw2&index=10
+ */
 class MainActivity : AppCompatActivity() {
     lateinit var database: ContactDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +24,11 @@ class MainActivity : AppCompatActivity() {
         GlobalScope.launch {
             database.contactDAO().insertContact(Contact(0,"Sid","9033762473"))
         }
+    }
+
+    fun getData(view: View) {
+        database.contactDAO().getContact().observe(this, Observer {
+            Log.d("database data", "getData: $it")
+        })
     }
 }
